5369303d0203734c737316d8a902d16851f90cab
Henning Jensen <henning@henning-laptop.(none)>
2009-03-30 00:39:13 +0200

    Plugin is now widget ready

--- wp-github-plugin.php
+++ wp-github-plugin.php
@@ -1,33 +1,59 @@
-<html>
-<head>
-  <title>wp-github-plugin</title>
-</head>
-<body>
+<?php
+/*
+	 Plugin Name: wp-github-plugin 
+	 Plugin URI: http://github.com/henningjensen/wp-github-plugin
+	 Description: Integrates with Github API and fetches a user's projects
+	 Author: Henning Jensen
+	 Version: 0.1
+	 Author URI: http://henning.fjas.no/blog
+ */
 
-  <script type="text/javascript" src="jquery-1.3.2.min.js"></script>
-  <script type="text/javascript">
-  var login = 'henningjensen';
-  var showForks = false;
-  var showPrivate = true;
-  var numberOfProjects = 10;
+function widget_github_register() {
+  function widget_github_project_list($args) {
+  	extract($args);
 
-  $.getJSON('http://github.com/api/v1/json/' + login + '?callback=?',
-    function(data) {
-      $.each(data.user.repositories, 
-        function(i){
-          if (!showForks && this.fork) return true;
-          if (!showPrivate && this.private) return true;
-          if (i == numberOfProjects) return false;
-          $('#repositories').append('<li><a href="' + this.url + '">' + this.name + '</a></li>');
-        }
-      );
-    }
-  );
-  </script>
+		// Pre-2.6 compatibility
+		if ( ! defined( 'WP_CONTENT_URL' ) ) 
+      define( 'WP_CONTENT_URL', get_option( 'siteurl' ) . '/wp-content' );
+		if ( ! defined( 'WP_CONTENT_DIR' ) ) 
+      define( 'WP_CONTENT_DIR', ABSPATH . 'wp-content' );
+		if ( ! defined( 'WP_PLUGIN_URL' ) ) 
+      define( 'WP_PLUGIN_URL', WP_CONTENT_URL. '/plugins' );
+		if ( ! defined( 'WP_PLUGIN_DIR' ) ) 
+      define( 'WP_PLUGIN_DIR', WP_CONTENT_DIR . '/plugins' );
 
-  <h3>Repositories</h3>
-  <ul id="repositories">
-  </ul>
+  	echo $before_widget; 
+  	echo $before_title . 'Github' . $after_title; 
+  ?>
 
-</body>
-</html>
+  	<script type="text/javascript" src="<?php echo WP_PLUGIN_URL; ?>/wp-github-plugin/jquery-1.3.2.min.js"></script>
+  	<script type="text/javascript">
+  	var login = 'henningjensen';
+  	var showForks = false;
+  	var showPrivate = true;
+  	var numberOfProjects = 10;
+  
+  	$.getJSON('http://github.com/api/v1/json/' + login + '?callback=?',
+  		function(data) {
+  			$.each(data.user.repositories, 
+  				function(i){
+  					if (!showForks && this.fork) return true;
+  					if (!showPrivate && this.private) return true;
+  					if (i == numberOfProjects) return false;
+  					$('#repositories').append('<li><a href="' + this.url + '">' + this.name + '</a></li>');
+  				}
+  			);
+  		}
+  	);
+  	</script>
+  
+  		<ul id="repositories"></ul>
+  <?php 
+  		echo $after_widget; 
+  }
+  
+  register_sidebar_widget('Github project list', 'widget_github_project_list');
+}
+add_action('plugins_loaded', 'widget_github_register');
+
+?>
